ffabf5783f4432d9b9aa724f5a2634dcde5ea3e9
tychxn <tychxn@gmail.com>
2018-06-09 15:39:14 +0800

    Enable login by QR code

--- JD_login.py
+++ JD_login.py
@@ -34,7 +34,7 @@ def response_status(resp):
 
 
 def get_current_time():
-    return time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime())
+    return time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime())
 
 
 def open_image(image_file):
@@ -46,13 +46,15 @@ def open_image(image_file):
         else:
             os.system("open " + image_file)  # for Mac
 
+def save_image(resp, image_file):
+    with open(image_file, 'wb') as f:
+        for chunk in resp.iter_content(chunk_size=1024):
+            f.write(chunk)
+
 
 class JDlogin(object):
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
+    def __init__(self):
         self.sess = requests.session()
         self.headers = {
             'Host': 'passport.jd.com',
@@ -97,17 +99,19 @@ class JDlogin(object):
 
         if not response_status(resp):
             print('获取验证码失败')
-            return False
+            return ''
 
-        with open (image_file, 'wb') as f:
-            for chunk in resp.iter_content(chunk_size=1024):
-                f.write(chunk)
+        save_image(resp, image_file)
         open_image(image_file)
         return input('验证码:')
 
+    def _get_login_page(self):
+        url = "https://passport.jd.com/new/login.aspx"
+        page = self.sess.get(url, headers=self.headers)
+        return page
+
     def _get_login_data(self):
-        home_url = "https://passport.jd.com/new/login.aspx"
-        page = self.sess.get(home_url, headers=self.headers)
+        page = self._get_login_page()
         soup = BeautifulSoup(page.text, "html.parser")
         input_list = soup.select('.form input')
 
@@ -121,32 +125,33 @@ class JDlogin(object):
         data['fp'] = '536e2679b85ddea9baccc7b705f2f8e0'
         return data
 
-    def login(self):
+    def login_by_username(self, username, password):
         data = self._get_login_data()
+        uuid = data['uuid']
 
         auth_code = ''
-        if self._need_auth_code(self.username):
+        if self._need_auth_code(username):
             print(get_current_time(), '本次登录需要验证码')
-            auth_code = self._get_auth_code(data['uuid'])
+            auth_code = self._get_auth_code(uuid)
         else:
             print(get_current_time(), '本次登录不需要验证码')
 
         login_url = "https://passport.jd.com/uc/loginService"
         payload = {
-            'uuid' : data['uuid'],
+            'uuid' : uuid,
             'version': 2015,
             'r': random.random(),
         }
         data['authcode'] = auth_code
-        data['loginname'] = self.username
-        data['nloginpwd'] = encrypt_pwd(self.password)
+        data['loginname'] = username
+        data['nloginpwd'] = encrypt_pwd(password)
         self.headers['Host'] = 'passport.jd.com'
         self.headers['Origin'] = 'https://passport.jd.com'
         self.headers['X-Requested-With'] = 'XMLHttpRequest'
-        resp = self.sess.post(login_url, data=data, headers=self.headers, params=payload)
+        resp = self.sess.post(url=login_url, data=data, headers=self.headers, params=payload)
 
         if not response_status(resp):
-            print('登录失败')
+            print(get_current_time(), '登录失败')
             return False
 
         if not self._get_login_result(resp):
@@ -164,9 +169,97 @@ class JDlogin(object):
             print(get_current_time(), '登录失败 '+ list(js.values())[0])
             return False
 
+    def _get_QRcode(self):
+        url = 'https://qr.m.jd.com/show'
+        self.headers['Host'] = 'qr.m.jd.com'
+        self.headers['Referer'] = 'https://passport.jd.com/new/login.aspx'
+        payload = {
+            'appid' : 133,
+            'size' : 147,
+            't': str(int(time.time() * 1000)),
+        }
+        resp = self.sess.get(url=url, headers=self.headers, params=payload)
+
+        if not response_status(resp):
+            print(get_current_time(), '获取二维码失败')
+            return False
+
+        QRCode_file = 'QRcode.png'
+        save_image(resp, QRCode_file)
+        print(get_current_time(), '验证码获取成功，请打开京东APP扫描')
+        open_image(QRCode_file)
+        return True
+
+    def _get_QRcode_ticket(self):
+        url = 'https://qr.m.jd.com/check'
+        payload = {
+            'appid': '133',
+            'callback' : 'jQuery{}'.format(random.randint(1000000, 9999999)),
+            'token' : self.sess.cookies.get('wlfstk_smdl'),
+            '_': str(int(time.time() * 1000)),
+        }
+        resp = self.sess.get(url=url, headers=self.headers, params=payload)
+
+        if not response_status(resp):
+            print(get_current_time(), '获取二维码扫描结果出错')
+            return False
+        js = json.loads(resp.text[14:-1])
+
+        if js['code'] != 200:
+            print(get_current_time(), 'Code: {0}, Message: {1}'.format(js['code'], js['msg']))
+            return None
+        else:
+            print(get_current_time(), '已完成手机客户端确认')
+            return js['ticket']
+
+    def _validate_QRcode_ticket(self, ticket):
+        url = 'https://passport.jd.com/uc/qrCodeTicketValidation'
+        self.headers['Host'] = 'passport.jd.com'
+        self.headers['Referer'] = 'https://passport.jd.com/uc/login?ltype=logout'
+        resp = self.sess.get(url=url, headers=self.headers, params={'t' : ticket})
+
+        if not response_status(resp):
+            return False
+
+        js = json.loads(resp.text)
+        if js['returnCode'] == 0:
+            return True
+        else:
+            print(get_current_time(), js)
+            return False
+
+    def login_by_QRcode(self):
+        self._get_login_page()
+
+        # download QR code
+        if not self._get_QRcode():
+            print(get_current_time(), '登录失败')
+            return False
+
+        # get QR code ticket
+        ticket = None
+        retry_times = 90
+        for _ in range(retry_times):
+            ticket = self._get_QRcode_ticket()
+            if ticket:
+                break
+            time.sleep(2)
+        else:
+            print(get_current_time(), '二维码扫描出错')
+            return False
+
+        # validate QR code ticket
+        if not self._validate_QRcode_ticket(ticket):
+            print(get_current_time(), '二维码登录失败')
+            return False
+        else:
+            print(get_current_time(), '二维码登录成功')
+            return True
+
 
 if __name__ == '__main__':
-    username = input('Username:')
-    password = input('Password:')
-    jd = JDlogin(username, password)
-    jd.login()
+    # username = input('Username:')
+    # password = input('Password:')
+    jd = JDlogin()
+    # jd.login_by_username(username, password)
+    jd.login_by_QRcode()
