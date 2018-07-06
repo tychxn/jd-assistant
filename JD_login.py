60b05666fe01517a91df8d4e617229d802667810
tychxn <tychxn@gmail.com>
2018-07-06 21:41:12 +0800

    Separate some functions into JD_tools.py

--- JD_login.py
+++ JD_login.py
@@ -5,68 +5,12 @@ import re
 import time
 import json
 import random
-from base64 import b64encode
 
 import requests
 import pickle
 from bs4 import BeautifulSoup
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
 
-rsa_public_key = """-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDC7kw8r6tq43pwApYvkJ5lalja
-N9BZb21TAIfT/vexbobzH7Q8SUdP5uDPXEBKzOjx2L28y7Xs1d9v3tdPfKI2LR7P
-AzWBmDMn8riHrDDNpUpJnlAGUqJG9ooPn8j7YNpcxCa1iybOlc2kEhmJn5uwoanQ
-q+CA6agNkqly2H4j6wIDAQAB
------END PUBLIC KEY-----"""
-
-
-def encrypt_pwd(password, public_key=rsa_public_key):
-    rsakey = RSA.importKey(public_key)
-    encryptor = Cipher_pkcs1_v1_5.new(rsakey)
-    cipher = b64encode(encryptor.encrypt(password.encode('utf-8')))
-    return cipher.decode('utf-8')
-
-
-def response_status(resp):
-    if resp.status_code != requests.codes.OK:
-        print('Status: %u, Url: %s' % (resp.status_code, resp.url))
-        return False
-    return True
-
-
-def get_current_time():
-    return time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime())
-
-
-def open_image(image_file):
-    if os.name == "nt":
-        os.system('start ' + image_file)  # for Windows
-    else:
-        if os.uname()[0] == "Linux":
-            os.system("eog " + image_file)  # for Linux
-        else:
-            os.system("open " + image_file)  # for Mac
-
-
-def save_image(resp, image_file):
-    with open(image_file, 'wb') as f:
-        for chunk in resp.iter_content(chunk_size=1024):
-            f.write(chunk)
-
-
-def parse_json(s):
-    begin = s.find('{')
-    end = s.rfind('}') + 1
-    return json.loads(s[begin:end])
-
-
-def get_tag_value(tag, key='', index=0):
-    if key:
-        value = tag[index].get(key)
-    else:
-        value = tag[index].text
-    return value.strip(' \t\r\n')
+from JD_tools import *
 
 
 class JDlogin(object):
@@ -427,14 +371,14 @@ class JDlogin(object):
 
 
 if __name__ == '__main__':
-    # username = input('Username:')
-    # password = input('Password:')
+    username = input('Username:')
+    password = input('Password:')
     jd = JDlogin()
-    # jd.login_by_username(username, password)
+    jd.login_by_username(username, password)
     # jd.login_by_QRcode()
     print(jd.get_item_stock_state(sku_id='5089267'))
     print(jd.get_item_price(sku_id='5089267'))
+    jd.clear_cart()
     jd.add_item_to_cart(sku_id='5089267')
-    # jd.clear_cart()
     jd.get_cart_detail()
     jd.get_checkout_page_detail()
